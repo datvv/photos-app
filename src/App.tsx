5cbcee45263516165f8976eeb3724ab17a1c4fc5
datvv <datvv>
2022-09-13 10:34:18 +0700

    init project (vitejs,react,tailwindcss)

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-[20px]">React + Tailwindss</div>
+    </div>
+  );
+}
+
+export default App;
